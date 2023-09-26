e30471c46102579e2e71f1a9bcb66ce2bc1250aa
NaimIbarra <80904524+NaimIbarra@users.noreply.github.com>
2023-09-26 18:58:02 -0300

    creacion de archivos

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const pg = require('pg');
+const path = require('path');
+const cookieParser = require('cookie-parser'); // Importa el módulo 'cookie-parser' para trabajar con cookies
+
+// Configura la conexión a la base de datos
+const pool = new pg.Pool({
+  user: 'postgres',
+  password: 'gxdfrf',
+  host: 'localhost',
+  database: 'postgres',
+  port: 5433,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser()); // Agrega el middleware de cookie-parser
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+  });
+  
+app.get("/register.html", (req, res) => {
+    res.sendFile(__dirname + '/register.html');
+  });
+
+app.get('/script-in-sesion.js', (req, res) => {
+    res.sendFile(__dirname + '/script-in-sesion.js', {
+      headers: {
+        'Content-Type': 'text/javascript'
+      }
+    });
+  });
+
+  app.get("/index.html", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+app.get("/adultos.html", (req, res) => {
+    res.sendFile(__dirname + '/adultos.html');
+  });
+
+  app.get("pediatria.html", (req, res) => {
+    res.sendFile(__dirname + 'pediatria.html');
+  });
+  app.get("/sala_prueba.html", (req, res) => {
+    res.sendFile(__dirname + '/sala_prueba.html');
+  });
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "style.css"));
+  });
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/script.js', {
+      headers: {
+        'Content-Type': 'text/javascript'
+      }
+    });
+  });
+
+app.post('/validar-inicio-sesion', (req, res) => {
+    const { usuario, contrasena } = req.body;
+  
+    pool.query('SELECT * FROM usuarios2 WHERE dni = $1 AND contraseña = $2', [usuario, contrasena], (error, result) => {
+      if (error) {
+        console.error('Error al consultar la base de datos:', error);
+        res.status(500).json({ error: 'Ocurrió un error al verificar las credenciales.' });
+      } else {
+        if (result.rows.length > 0) {
+          const usuario = result.rows[0];
+          // Almacena el rol del usuario en una cookie
+          res.cookie('rol', usuario.rol);
+          res.json({ mensaje: 'Credenciales válidas. Inicio de sesión exitoso.' });
+        } else {
+            res.status(401).json({ error: 'Credenciales inválidas. Inicio de sesión fallido.' });
+        }
+      }
+    });
+  });
+
+app.get('/consultar-datos', (req, res) => {
+  pool.query('SELECT * FROM usuarios', (error, result) => {
+    if (error) {
+      console.error('Error al consultar la base de datos:', error);
+      res.status(500).json({ error: 'Ocurrió un error al consultar la base de datos.' });
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en funcionamiento en http://localhost:${port}`);
+});
---
+++ script-in-sesion.js
@@ -0,0 +1,78 @@
+
+// Obtén una referencia al formulario de inicio de sesión
+const formularioInicioSesion = document.getElementById('formularioInicioSesion');
+
+// Agrega un evento de envío al formulario
+formularioInicioSesion.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+    // Obtiene los valores de correo electrónico y contraseña ingresados por el usuario
+    const usuario = document.getElementById('usuario').value;
+    const contrasena = document.getElementById('contrasena').value;
+
+    // Realiza una solicitud al servidor para validar las credenciales
+    validarCredenciales(usuario, contrasena);
+});
+// Obtener el valor de la cookie "rol"
+const rol = getCookie('rol');
+
+// Función para obtener el valor de una cookie por su nombre
+function getCookie(name) {
+  const cookieName = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return "";
+}
+
+// Ahora puedes usar el valor de 'rol' para personalizar la funcionalidad
+if (rol === 'administrador') {
+  // Aquí agregas código específico para administradores
+  console.log('Usuario administrador');
+} else if (rol === 'usuario_generico') {
+  // Aquí agregas código específico para usuarios genéricos
+  console.log('Usuario genérico');
+} else {
+  // Manejar otros casos si es necesario
+  console.log('Rol desconocido o no autenticado');
+}
+
+function validarCredenciales(usuario, contrasena) {
+    // Realiza una solicitud POST al servidor para validar las credenciales
+    fetch('/validar-inicio-sesion', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ usuario, contrasena }), // Envía los datos al servidor en formato JSON
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Muestra un mensaje al usuario
+            if (data.mensaje) {
+                alert(data.mensaje);
+            } else {
+                alert('Credenciales inválidas. Inicio de sesión fallido.');
+            }
+        
+            // Cuando las credenciales son válidas, redirige al usuario a index.html
+            if (data.mensaje === 'Credenciales válidas. Inicio de sesión exitoso.') {
+                window.location.href = '/index.html';
+            }
+        })
+        
+            // Puedes redirigir al usuario a otra página aquí si lo deseas
+
+        .catch(error => {
+            console.error('Error al validar credenciales:', error);
+            alert('Ocurrió un error al iniciar sesión.');
+        });
+} 
